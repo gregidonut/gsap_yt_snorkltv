a62f05bb1c1a9acb4513d71c4f6757ab5eb84da8
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2025-05-21 02:06:01 +0800

    chore: add some boilerplate
    i might as well just start with react the vanilla javascript api i just
    too much work

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import compressor from 'astro-compressor';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compressor()]
+  integrations: [compressor(), react()]
 });
---
+++ src/pages/reactSection/_ReactSection.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ReactSection(): React.JSX.Element {
+    return <h2>ulol</h2>;
+}
