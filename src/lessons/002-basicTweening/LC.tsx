3d85220b76e72a9080891dc5918bb4ae5a12cb70
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2025-05-21 03:42:08 +0800

    feat: add lesson 2
    can't get any more basic than this

---
+++ src/lessons/002-basicTweening/LC.tsx
@@ -0,0 +1,21 @@
+import React, { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
+export default function LessonComponent(): React.JSX.Element {
+    const container = useRef(null);
+    useGSAP(
+        function () {
+            gsap.to("img", { x: 400, y: 350, scale: 3, duration: 3 });
+        },
+        { scope: container },
+    );
+
+    return (
+        <section ref={container}>
+            <img src="/sword.svg" alt="sword" height={75} />
+        </section>
+    );
+}
